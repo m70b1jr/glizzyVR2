4d9c9f6776294c459ec010d0135f80ad41458752
m70b1jr <corbin.titus@my.lr.edu>
2020-10-21 17:28:16 -0400

    Huge update

---
+++ Assets/SmoothMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+//Script for our continous movement system
+//Teleport is disabled since it's a MP Game, and teleport is gay
+
+public class SmoothMovement : MonoBehaviour
+{
+    //Variables
+    //This is our speed variable, it needs to be able to be changed for different movement types
+    public float speed = 1;
+    //Controller or HMD
+    public XRNode inputSource;
+    //Our gravity, or players will just float in the air
+    public float gravity = -9.81f;
+    private float fallingSpeed;
+    //Our ground layer var
+    public LayerMask groundLayer;
+    //Variable so our capsule sticks with the IRL HMD
+    public float additionalHeight = 0.2f;
+    //Our rig variable
+    private XRRig rig;
+    //axis input
+    private Vector2 inputAxis;
+    //our character variable
+    private CharacterController character;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        //gravity system
+        bool isGrounded = onGround();
+        if (isGrounded)
+            fallingSpeed = 0;
+        else
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    //This function is so the ingame capsule will follow the players HMD in real life.
+    void CapsuleFollowHeadset()
+    {
+      //  character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
+
+    //check if we're on the ground
+    bool onGround()
+        {
+            Vector3 rayStart = transform.TransformPoint(character.center);
+            float rayLength = character.center.y + 0.01f;
+            bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+            return hasHit;
+        }
+
+}
---
+++ Assets/SmoothTurnProvider.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SmoothTurnProvider : LocomotionProvider
+{
+    // How much do we turn?
+    public float turnSegment = 45.0f;
+
+    // How long does it take to turn?
+    public float turnTime = 3.0f;
+
+    // Basic input - I'd recommend replacing with an input solution.
+    public InputHelpers.Button rightTurnButton = InputHelpers.Button.PrimaryAxis2DRight;
+    public InputHelpers.Button leftTurnButton = InputHelpers.Button.PrimaryAxis2DLeft;
+
+    // List of the controllers we're going to use
+    public List<XRController> controllers = new List<XRController>();
+
+    // The amount we're turning to
+    public float TargetTurnAmount = 0.0f;
+
+    private void Update()
+    {
+        // Let's ask the locomotion system if we can move
+        if (CanBeginLocomotion())
+            CheckForInput();
+    }
+
+    private void CheckForInput()
+    {
+        // Go through the controllers
+        foreach (XRController controller in controllers)
+        {
+            TargetTurnAmount = CheckForTurn(controller);
+
+            if (TargetTurnAmount != 0.0f)
+                TrySmoothTurn();
+        }
+
+    }
+
+    // Check for input, this can be done cleaner with a more robust input solution
+    private float CheckForTurn(XRController controller)
+    {
+        if (controller.inputDevice.IsPressed(rightTurnButton, out bool rightPress))
+        {
+            // Check if we pressed right
+            if (rightPress)
+                return turnSegment;
+        }
+
+        if (controller.inputDevice.IsPressed(leftTurnButton, out bool leftPress))
+        {
+            // Check if we pressed left
+            if (leftPress)
+                return -turnSegment;
+        }
+
+
+        // If we hit nothing return 0
+        return 0.0f;
+    }
+
+    private void TrySmoothTurn()
+    {
+        // Let's stry turning with the amount we got
+        StartCoroutine(TurnRoutine(TargetTurnAmount));
+
+        // Since the value has been used, let's clear it out
+        TargetTurnAmount = 0.0f;
+    }
+
+    private IEnumerator TurnRoutine(float turnAmount)
+    {
+        // We need to store this since we only want to pass the difference
+        float previousTurnChange = 0.0f;
+
+        // Record the whole time of the loop for proper lerp
+        float elapsedTime = 0.0f;
+
+        // Let the motion begin
+        BeginLocomotion();
+
+        while (elapsedTime <= turnTime)
+        {
+
+            // How far are we into the lerp?
+            float blend = elapsedTime / turnTime;
+            float turnChange = Mathf.Lerp(0, turnAmount, blend);
+
+            // Figure out the difference and apply it
+            float turnDifference = turnChange - previousTurnChange;
+            system.xrRig.RotateAroundCameraUsingRigUp(turnDifference);
+
+            // Save the current amount we've moved, and add to elapsed time
+            previousTurnChange = turnChange;
+            elapsedTime += Time.deltaTime;
+
+            // Yield or we're crashing
+            yield return null;
+        }
+        // Let the motion end
+        EndLocomotion();
+    }
+}
